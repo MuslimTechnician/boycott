bbb7b250d513a6f1e4833a98ea87b10526d87a0f
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-11-10 21:36:40 +0600

    code formatted.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
-    return (
-        <div className="container mx-auto">
-            <p>Boycott Project</p>
-        </div>
-    );
+  return (
+    <div className="container mx-auto">
+      <p>Boycott Project</p>
+    </div>
+  );
 }
